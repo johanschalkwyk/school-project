9e5f94623c04b14e1f8dbc7704a6293090196b69
Johan Schalkwyk <johan.schalkwyk@gmail.com>
2017-05-31 15:53:24 -0400

    A School Object Orientated project

---
+++ school.js
@@ -0,0 +1,26 @@
+//
+// A program to keep track of students, their classes and the teachers who
+// teach team
+//
+// Basic class design
+//
+//   A Person
+//        hasa firstname, lastname
+//
+//   A Student isa Person
+//        hasa student-id
+//        hasa (attends) classes[]
+//
+//   A Teacher isa Person
+//        hasa teacher-id (drivers-license)
+//        hasa (teaches) classes[]
+//
+//   A class
+//        hasa name
+//        hasa Teacher
+//        hasa Students[]
+//
+//  Write a program that builds a Math Class, History class, with
+//  Students enrolled in each class, then when all students are added
+//  print the details of each class (student, teacher)
+//
