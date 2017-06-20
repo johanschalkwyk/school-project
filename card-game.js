67b2a420f47125f29088517f39c832569fd8fa2c
Johan Schalkwyk <johans@google.com>
2017-06-20 17:34:29 -0400

    Card game cleanup.

--- card-game.js
+++ card-game.js
@@ -65,89 +65,86 @@ class Deck {
 class Model {
   constructor() {
     this.deck = new Deck();
+    this.deck.shuffle();
     this.deck.deal(1);
   }
 
   deal() {
-    this.hand = deck.deal(7);
+    this.hand = this.deck.deal(7);
     console.log(this.hand);
   }
+
+  gofish() {
+    var card = this.deck.deal(1);
+    this.hand.push(card);
+    return card[0];
+  }
+
+  getHand() { return this.hand; }
 }
 
 class View {
-  constructor(model) {
-    this.model = model;
-    this.gallery = $("#gallery");
-    this.cardgroup = $("#cardgroup");
-    showHand();
+  constructor() {
+    this.handview   = $("#hand");
+    this.cardgroups = $("#cardgroups");
   }
 
-  showHand() {
-
-  // Add cards to the gallery
-  var i;
-  for (i=0; i < hand.length; i++) {
-    var html = "<li class=\"ui-widget-content ui-corner-tr\">" +
-	"<img name=\"" + i.toString() + "\" src=\"" + hand[i].img +
-	"\" width=\"72px\" height=\"96px\"></img>" + "</li>";
+  addCard(card) {
+    var html = "<div name=\"" + card.getValue() +
+	"\" class='card ui-widget'><img class='cardimage' src='img/" +
+	card.getValue() + ".png'></div>";
     console.log(html);
-    gallery.append(html);
+
+    var newHtml = this.handview.html() + html;
+    this.handview.empty().append(newHtml);
   }
 
-  // Let the gallery items be draggable
-  $( "li", gallery ).draggable({
-    cancel: "a.ui-icon", // clicking an icon won't initiate dragging
-    revert: "invalid",   // when not dropped, the item will revert back
-                         // to its initial position
-    containment: "document",
-    helper: "clone",
-    cursor: "move"
-  });
-
-  // Let the cardgroup be droppable, accepting the gallery items
-  cardgroup.droppable({
-    accept: "#gallery > li",
-    classes: {
-      "ui-droppable-active": "ui-state-highlight"
-    },
-    drop: function( event, ui ) {
-      moveCard( ui.draggable );
+  showHand(hand) {
+    // Add cards to the gallery
+    this.handview.empty();
+    var i;
+    for (i=0; i < hand.length; i++) {
+      this.addCard(hand[i]);
     }
-  });
-
-  // Let the gallery be droppable as well, accepting items from the cardgroup
-  gallery.droppable({
-    accept: "#cardgroup li",
-    classes: {
-      "ui-droppable-active": "custom-state-active"
-    },
-    drop: function( event, ui ) {
-      revertCard( ui.draggable );
-    }
-  });
-
-  function moveCard( item ) {
-    var name = item.find("img").attr("name");
-    console.log("card: " + hand[parseInt(name)].getValue());
-
-    item.fadeOut(function() {
-      var list = $( "ul", cardgroup ).length ?
-          $( "ul", cardgroup ) :
-          $( "<ul class='gallery ui-helper-reset'/>" ).appendTo(cardgroup);
-
-      item.appendTo(list).fadeIn(function() {
-        item.find( "img" );
-      });
-    });
+    sleep(10);
   }
 
-  function revertCard( item ) {
-    item.fadeOut(function() {
-      item
-          .find( "img" )
-          .end()
-          .appendTo( gallery )
-          .fadeIn();
-    });
+  updateDraggable() {
+    $( "#hand div.card" ).draggable();
   }
-} );
+
+  updateDroppable() {
+    $( "#cardgroups div.groupbox" ).droppable({
+      accept: "#hand div.card",
+      classes: {
+        "ui-droppable-active": "ui-state-active",
+        "ui-droppable-hover": "ui-state-hover"
+      },
+      drop: function( event, ui ) {
+        $( this ).addClass( "ui-state-highlight" )
+        console.log("drop box name: " + $(this).attr("name"));
+        console.log("card name    : " + ui.draggable.attr("name"));
+     }
+   });
+  }
+}
+
+model = new Model();
+
+// controller
+$(document).ready(function() {
+  view  = new View();
+
+  $("#newgame").click(function() {
+    model.deal();
+    view.showHand(model.getHand());
+    view.updateDraggable();
+    view.updateDroppable();
+  });
+
+  $("#gofish").click(function() {
+    view.addCard(model.gofish());
+    view.updateDraggable();
+    view.updateDroppable();
+  });
+});
